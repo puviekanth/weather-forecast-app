1292bd39faa86b7ae94b9b19a6bca1f06c76ca35
Puviekanth Balraj <puvie9edx@gmail.com>
2025-01-11 14:57:43 +0530

    Done the theme toggler

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import { useContext } from 'react';
 import Header from './components/Header'
 import Main from './components/Main'
 import './styles/components/App.scss'
 import 'bootstrap-icons/font/bootstrap-icons.css'
-
+import ThemeContext from './context/themes.context'
 function App() {
 
-  const dark = true;
+  const {dark} = useContext(ThemeContext);
   
   return (
     <>
