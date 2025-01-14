4124d93f18ae85536cb0b58603077dcbf8af1a03
Puviekanth Balraj <puvie9edx@gmail.com>
2025-01-14 09:49:25 +0530

    Done the seach city and rendering the results

--- src/api/index.js
+++ src/api/index.js
@@ -25,4 +25,26 @@ try {
 }
 }
 
+export async function searchPlaces(text){
+  const options = {
+    method: 'GET',
+    url: 'https://ai-weather-by-meteosource.p.rapidapi.com/find_places',
+    params: {
+      text: text,
+      language: 'en'
+    },
+    headers: {
+      'x-rapidapi-key': '059f4bb93amsh181497324054852p14013djsn368e80703ca9',
+      'x-rapidapi-host': 'ai-weather-by-meteosource.p.rapidapi.com'
+    }
+  };
+  
+  try {
+    const response = await axios.request(options);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
  
